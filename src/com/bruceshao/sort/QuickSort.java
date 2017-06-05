8f71948b13a49c209782d340b340d69b1a16d1e7
hhszg <szg@haohandata.com.cn>
2017-06-05 13:13:48 +0800

    增加快排代码

--- src/com/bruceshao/sort/QuickSort.java
+++ src/com/bruceshao/sort/QuickSort.java
@@ -21,9 +21,39 @@ public class QuickSort
 {
     public static void main(String[] args)
     {
-        
+        int[] nums = {1,3,6,7,9,5,4,2,8};
+        quickSort(nums, 0 , nums.length - 1);
+        for (int i : nums)
+        {
+            System.out.print(i + " ");
+        }
     }
     
-    
+    public static void quickSort(int[] nums, int left, int right)
+    {
+        if (left >= right)
+        {
+            return;
+        }
+        int i = left;
+        int j = right;
+        int key = nums[left];
+        while (i < j)
+        {
+            while (i < j && nums[j] >= key)
+            {
+                j--;
+            }
+            nums[i] = nums[j];
+            while (i < j && nums[i] <= key)
+            {
+                i++;
+            }
+            nums[j] = nums[i];
+            nums[i] = key;
+            quickSort(nums, left, i - 1);
+            quickSort(nums, i + 1, right);
+        }
+    }
     
 }
